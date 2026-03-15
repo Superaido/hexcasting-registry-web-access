2bd7bc9c3a259848741fa5b99356a4b663878678
Adrianne Odette <odetteclan1@gmail.com>
2026-03-15 14:56:36 -0400

    added /get-id endpoint in order to get a pattern's id from its name. moved pattern json search to /get-data endpoint

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ app = FastAPI()
 with open("hexbug_registry.json", encoding="utf-8") as f:
     DATA = json.load(f)
 
+@app.get("/get-id")
+def get_id(name: str):
+    for pattern in DATA["patterns"]:
+        if pattern["name"] == name:
+            return pattern["id"]
 
-@app.get("/{path:path}")
+@app.get("get-data/{path:path}")
 def get_data(path: str):
     # Split the URL path into keys
     keys = [k for k in path.strip("/").split("/") if k]
